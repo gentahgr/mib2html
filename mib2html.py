6c253cf4cd48cd33c8e95f70af48944ac4534ab6
Gentaro <genta.hgr@gmail.com>
2013-05-21 21:53:58 +0900

    Add row description

--- mib2html.py
+++ mib2html.py
@@ -377,6 +377,22 @@ def fl_parse_scalar(node):
 
     return result
 
+def fl_parse_row(node):
+    """parse for row
+    input:
+        node: (Element)
+    return:
+        list of tuple
+        [ (param1, value1), (param2, value2),...]
+        most significant elemnt shall be the fist element of the list.
+        The value of the first element is shown for short-form.
+    """
+    result = [
+        (u"oid", node.get(u"oid")),
+        (u"status", node.get(u"status", u"current"))
+        (u"create", node.get(u"create", u"true"))
+        ]
+    
 
 def fl_parse_table(node):
     """parse table #1 : description part
